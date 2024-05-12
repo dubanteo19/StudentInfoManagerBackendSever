ede0993bfd02da3a38caa968e9ed968aa8b703eb
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2024-05-12 23:46:03 +0700

    update student api add date format

--- src/main/java/com/cnpm/StudentInfoManager/students/Student.java
+++ src/main/java/com/cnpm/StudentInfoManager/students/Student.java
@@ -2,8 +2,10 @@ package com.cnpm.StudentInfoManager.students;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.sql.Date;
+
 
 @Data
 @Entity
@@ -11,11 +13,12 @@ import java.sql.Date;
 public class Student{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long studentId;
+    private Long studentId;
     private String name;
     private String email;
     private String phone;
     private String address;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
     private String gender;
     private String avatar;
